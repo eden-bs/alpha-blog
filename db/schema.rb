6ac23c59e90a00a74abccdb33b37c5e38755103f
eden-bs <edenbs1997@gmail.com>
2025-01-12 23:01:09 +0200

    added article show by index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_12_192340) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_12_203527) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
