41a6eb6d7df52eda1e9878b89a09131c3648d679
eden-bs <edenbs1997@gmail.com>
2025-03-23 20:43:34 +0200

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
   def new
       @user = User.new
   end
@@ -22,7 +26,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flas[:notice] = "Your account info was successfully updated"
+      flash[:notice] = "Your account info was successfully updated"
       redirect_to articles_path
     else
       render "edit"
