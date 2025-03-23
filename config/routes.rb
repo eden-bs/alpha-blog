d1871bc829d020085e003837810e33f07b1234a5
eden-bs <edenbs1997@gmail.com>
2025-03-23 10:38:48 +0200

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "about", to: "pages#about"
   resources :articles
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
